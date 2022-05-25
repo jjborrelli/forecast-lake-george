1f2dd29cffdf833167701cc2fb66355764c48489
jjborrelli <borrejj@gmail.com>
2022-05-25 16:55:38 -0400

    start script to generate forecast report - added required libs

---
+++ make_report.R
@@ -0,0 +1,20 @@
+# Required Libraries
+## simulations
+library(Rnoaa4cast)
+library(LakeEnsemblR)
+library(gotmtools)
+library(rLakeAnalyzer)
+
+## data wrangling
+library(ncdf4)
+library(lubridate)
+library(dplyr)
+library(tidyr)
+
+## plotting
+library(ggplot2)
+library(patchwork)
+
+## reporting
+library(emayili)
+library(rmarkdown)
